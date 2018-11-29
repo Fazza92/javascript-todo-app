890e137e13b522596316b2058b788eae3ce3443c
Oscar <oscarwasil@gmail.com>
2018-11-28 20:29:50 -0600

    Updated to version 2

--- javascript-todo-app.js
+++ javascript-todo-app.js
@@ -1,20 +1,19 @@
-var todos = ['item 1', 'item 2', 'item 3']
+var todoList = {
+  todos: ['item 1', 'item 2', 'item 3'],
+  displayTodos: function() {
+    console.log('My Todos', this.todos);
+  },
+  addTodo: function(todo) {
+    this.todos.push(todo);
+    this.displayTodos();
+  },
+  changeTodo: function(position, newValue) {
+    this.todos[position] = newValue;
+    this.displayTodos();
+  },
+  deleteTodo: function(position) {
+    this.todos.splice(position, 1);
+    this.displayTodos();
+  }
+};
 
-function displayTodos() {
-  console.log('My todos:', todos);
-}
-
-function addTodo() {
-  todos.push('new todo');
-  displayTodos();
-}
-
-function changeTodo() {
-  todos[position] = newValue;
-  displayTodos();
-}
-
-function deleteTodo(position) {
-  todos.splice(position, 1);
-  displayTodos();
-}
